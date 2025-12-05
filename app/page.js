8c7f322e19be2b25e411d297d73821985988fc53
Sinan TK <your-email@example.com>
2025-12-05 10:58:31 +0530

    change

--- app/page.js
+++ app/page.js
@@ -1,19 +1,23 @@
 import Productlist from "./components/productlist";
-import Carousel from "./components/Carousel"
+import Carousel from "./components/Carousel";
+
 async function getProducts() {
-  const res = await fetch("https://fakestoreapi.com/products");
+  const res = await fetch("https://fakestoreapi.com/products", { cache: "no-store" });
+  if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 }
+
 export const metadata = {
   title: "STYLE HUB",
   description: "This is the description of the page",
 };
+
 export default async function Home() {
   const products = await getProducts();
 
   return (
     <div>
-<Carousel/>
+      <Carousel />
       {/* ✅ Only 4 products */}
       <Productlist products={products} limit={4} />
     </div>
